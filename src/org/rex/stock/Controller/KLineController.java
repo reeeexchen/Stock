371e680e51c86afce23d2ec6f64de88fea7c1beb
REEEEX <rexchen1997@yahoo.com>
2018-04-17 10:37:28 +0800

    Version 1.0.1
    更新K线图自动更新 5秒一次

--- src/org/rex/stock/Controller/KLineController.java
+++ src/org/rex/stock/Controller/KLineController.java
@@ -1,14 +1,21 @@
 package org.rex.stock.Controller;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import jdk.internal.util.xml.impl.Input;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -21,37 +28,69 @@ import org.apache.http.impl.client.HttpClients;
  * @Date: Create in 20:00 2018/4/13
  */
 public class KLineController {
-	public void show(String id) throws IOException {
-		//System.out.println("id = " + id);
-		GridPane gridPane = new GridPane();
-		ImageView imageView = new ImageView();
+    public void show(String id) throws IOException {
+        //System.out.println("id = " + id);
+        GridPane gridPane = new GridPane();
+        ImageView imageView = new ImageView();
+        final String ID = id;
 
-		InputStream inputStream = getGif(id);
-		Image image = new Image(inputStream);
-		imageView.setImage(image);
+        InputStream inputStream = getGif(ID);
+        Image image = new Image(inputStream);
+        imageView.setImage(image);
 
-		gridPane.add(imageView, 0, 0);
-		Stage stage = new Stage();
-		stage.setScene(new Scene(gridPane));
-		stage.show();
-	}
+        Button refresh = new Button("自动刷新");
+        Button close = new Button("关闭窗口");
+        VBox vBox = new VBox();
+        vBox.setSpacing(20);
+        vBox.getChildren().addAll(refresh, close);
+        vBox.setAlignment(Pos.CENTER);
 
-	public InputStream getGif(String s) throws IOException {
-		CloseableHttpClient closeableHttpClient= HttpClients.createDefault(); //1、创建实例
-		String url = "http://image.sinajs.cn/newchart/min/n/"+ s + ".gif";
-		System.out.println(url);
-		HttpGet httpGet=new HttpGet(url); //2、创建请求
+        gridPane.add(imageView, 0, 0);
+        gridPane.add(vBox, 1, 0);
 
-		CloseableHttpResponse closeableHttpResponse=closeableHttpClient.execute(httpGet); //3、执行
-		HttpEntity httpEntity=closeableHttpResponse.getEntity(); //4、获取实体
+        Stage stage = new Stage();
+        stage.setScene(new Scene(gridPane));
+        stage.show();
 
-		if(httpEntity != null){
-			// System.out.println("ContentType:"+httpEntity.getContentType().getValue());
-			InputStream inputStream = httpEntity.getContent();
-			return inputStream;
-		}
-		closeableHttpResponse.close();
-		closeableHttpClient.close();
-		return null;
-	}
+        // BTN METHOD
+        close.setOnAction(e -> {
+            stage.close();
+        });
+
+        refresh.setOnAction(e -> {
+            Timer re_timer = new Timer();
+            re_timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    InputStream inputStream1 = null;
+                    try {
+                        inputStream1 = getGif(ID);
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                    Image image1 = new Image(inputStream1);
+                    imageView.setImage(image1);
+                }
+            }, 1000, 5000);
+        });
+    }
+
+    public InputStream getGif(String s) throws IOException {
+        CloseableHttpClient closeableHttpClient = HttpClients.createDefault(); //1、创建实例
+        String url = "http://image.sinajs.cn/newchart/min/n/" + s + ".gif";
+        System.out.println(url);
+        HttpGet httpGet = new HttpGet(url); //2、创建请求
+
+        CloseableHttpResponse closeableHttpResponse = closeableHttpClient.execute(httpGet); //3、执行
+        HttpEntity httpEntity = closeableHttpResponse.getEntity(); //4、获取实体
+
+        if (httpEntity != null) {
+            // System.out.println("ContentType:"+httpEntity.getContentType().getValue());
+            InputStream inputStream = httpEntity.getContent();
+            return inputStream;
+        }
+        closeableHttpResponse.close();
+        closeableHttpClient.close();
+        return null;
+    }
 }
